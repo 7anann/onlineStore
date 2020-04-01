666901c7d7ce2b7343bba809ce4222581c0addde
Hanan Mohamed <36165902+7anann@users.noreply.github.com>
2020-04-01 04:20:58 +0200

    added empty project and ignore/attributes files

---
+++ sweProject/sweProject/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace sweProject.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ sweProject/sweProject/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace sweProject.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
