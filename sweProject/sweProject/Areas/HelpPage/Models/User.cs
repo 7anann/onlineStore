1c512239cfafa1a2a490b3a341040dbb315a762b
Hanan Mohamed <36165902+7anann@users.noreply.github.com>
2020-04-02 20:49:58 +0200

    Update User.cs
    Added get all users function in User class, Models

--- sweProject/sweProject/Areas/HelpPage/Models/User.cs
+++ sweProject/sweProject/Areas/HelpPage/Models/User.cs
@@ -20,6 +20,25 @@ namespace sweProject.Models
             db.SubmitChanges();
             return true;
         }
+        
+          public static List<User> getAllUsers()
+        {
+            usersDatabaseDataContext db = new usersDatabaseDataContext();
+            storeTable obj = new storeTable();
+            List<storeTable> list = (from u in db.storeTables select u).ToList<storeTable>();
+            List<User> users = new List<User>();
+            for (int i = 0; i < list.Count; i++)
+            {
+                User u = new User();
+                 u.username = list[i].username;
+                 u.password = "***";
+                 u.email = list[i].email;
+                 users.Add(u);
+
+            }
+
+            return users;
+        }
 
     }
 }
