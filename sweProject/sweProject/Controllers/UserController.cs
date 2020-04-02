e1faaf4cefffa563f9235085bf4bdb13c374448f
Hanan Mohamed <36165902+7anann@users.noreply.github.com>
2020-04-02 19:47:33 +0200

    Add files via upload
    added user controller

---
+++ sweProject/sweProject/Controllers/UserController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using sweProject.Models;
+
+
+namespace sweProject.Controllers
+{
+    public class UserController : ApiController
+    {
+       
+    }
+}
+
