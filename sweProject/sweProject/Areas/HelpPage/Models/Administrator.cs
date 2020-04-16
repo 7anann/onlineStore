5e95c66bef45b217e7978880054ed6ba478e55bd
Hanan Mohamed <36165902+7anann@users.noreply.github.com>
2020-04-16 19:17:22 +0200

    Update Administrator.cs
    Added get all users into Administrator class

--- sweProject/sweProject/Areas/HelpPage/Models/Administrator.cs
+++ sweProject/sweProject/Areas/HelpPage/Models/Administrator.cs
@@ -7,5 +7,25 @@ namespace sweProject.Models
 {
     public class Administrator
     {
+        public static List<User> getAllUsers()
+        {
+            usersDatabaseDataContext db = new usersDatabaseDataContext();
+            storeTable obj = new storeTable();
+            List<storeTable> list = (from u in db.storeTables select u).ToList<storeTable>();
+            List<User> users = new List<User>();
+            for (int i = 0; i < list.Count; i++)
+            {
+                User u = new User();
+                u.username = list[i].username;
+                u.password = list[i].password;
+                u.email = list[i].email;
+                u.type = list[i].type;
+
+                users.Add(u);
+
+            }
+
+            return users;
+        }
     }
 }
