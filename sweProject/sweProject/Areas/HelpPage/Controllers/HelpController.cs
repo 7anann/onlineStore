313411e70233fd975fbf1125d633da5ad99a5608
rana2227 <44726377+rana2227@users.noreply.github.com>
2020-04-17 18:18:09 +0200

    Update HelpController.cs

--- sweProject/sweProject/Areas/HelpPage/Controllers/HelpController.cs
+++ sweProject/sweProject/Areas/HelpPage/Controllers/HelpController.cs
@@ -23,12 +23,7 @@ namespace sweProject.Areas.HelpPage.Controllers
             Configuration = config;
         }
         
-        [HttpPost]
-        [Route("login/")]
-        public IHttpActionResult logIn(User u)
-        {         
-            return Ok(u.logIn(u));
-        }
+        
 
         public HttpConfiguration Configuration { get; private set; }
 
